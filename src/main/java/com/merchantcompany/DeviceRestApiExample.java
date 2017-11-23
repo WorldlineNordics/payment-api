ba8afb39e13b9fd30d26d8e7c00f708e5b29413f
Rickard Schoultz <rschoultz@digitalriver.com>
2017-11-23 20:34:57 +0100

    Updated old example code

--- src/main/java/com/merchantcompany/DeviceRestApiExample.java
+++ src/main/java/com/merchantcompany/DeviceRestApiExample.java
@@ -25,21 +25,15 @@ public class DeviceRestApiExample {
 
 	public static void main(String[] args) {
 
-		PaymentHandler paymentHandler = new PaymentHandler(
-				new JKSKeyHandlerV6("src/test/resources/netgiro-2048.jks", "123456", "netgiro", "netgiro"));
+        PaymentHandler paymentHandler = new PaymentHandler(new JKSKeyHandlerV6("src/test/resources/merchant.jks", "merchant", "merchant", "drwp_cert"));
 
 		PaymentRequest request = buildRequest();
-		
 		String encryptedPayload = paymentHandler.encryptRequest(request);
-
 		System.out.println(encryptedPayload);
 
-		// sendRedirect(redirectUrl);
-		// get back a response when consumer comes back
 
-		String respString = "BgAAA-wAAAPqHZ-c1hJnrCiLG-JCvx7A-0lOGYWfYN6Uf8qH8Si15_22TvuQYX01VSCPsJNI5P_OkwtYv1xDhCmW8FPJora34L9bnL2UKSY9Zx8YpPjrCM3dTvM_ftoSDB_FXzlbkB6bwp_Vuc5U3L8FDbxAh6l3H1PZ3sjwe7Ew3Fx2qVv5DapTTuJ_RfyUCXi1w-ktNK0hIglACEg8H7zxMhqaLC15_O9_fYpqKBOgvvpTQyB3cBW2L7k-QhBF5SU1rKo_tAh56AtTMpJ1iojWHF4lCYykzR-MXzfThgZ0UxHheN9VYLL2r46XL7Zen3bv3t6_fQ4YFW37RR21THvhI0ho4JwKh0Q_pQ7qQkUjEGVR33MT88bIsH85-0iC_KrwVhFWbk-gYVPE6JnNy9FuSMkQ7Ax8MDBKkImJHqMgkJl-22UqihiYymYKl_90rLNjLlx7b0n2Mfa6yrJT4D8wRS5ZavrOISoupL9J5W9czs7g7oeLU7XoJEfz2efwTpamD90JxTokqmPr8vt-TWRNcZanKbAXQ-K7C7VZk5F28aGWBTeIEfTce7OTDusLpneRYOWQnOXAPMMkCbGGrID43tmBfIdhSmXCagqWU0ojSkdalRBZtvpE83Amjf_EvV6HE13KXykhZH5sH04w-a8bqfl8NLzpzgb-dCs8bFSFv8afKznobazDmEPjHygFqiW3fhdRqZdClO2CZtJedKajqudSoOyw9T_PVpju3l2R3cgPeIgUweh9i0BX4Q8iPqE6WDe8K2fibNxR18cgZ0yb5W4jofI_Mg919TMOfHdNonLT_lbS63cFjpaf9TUyt0ghkU16O3BhX7nN_tAzZbrD_sO-c9C3dcvMHBLmwII0OMgRTL9fvS4=";
+        String respString = "BgAAA-sAAAPqLmG9awk-ZuE00dxJc0fjlsCQsSTSSfpRdCtrGIMXnhIBHGm-gnhdFG0vKX_t1CttI1Ss2ySGScKgDJPYhxO6wcjL22u932xZAdN72fo5vFq-3pdm5SSehDCLv0IWj5qfwBzPCB2QCE_kW-N-HbrRpjYN6c3h-Kv_h0kVRESPQd7pSaL86XRfl02f-peCThc4KpHMPRBUCVVjPQQfOJx6YqrNqmBjcTorrCzNlDXRruGIdlgrPxyKr2TmyhKs-v-05G_4B4ZEdkSrmun1ulDoVFYlzcTuXGr4o-mwHG8ZrLpLWYL47ODUTxgyOghJe6lw7IsB4fI9kU2ka7-unAj5CmIoI5lW7TjNNqkU_jUIDkq40WxuMHJujWuyO1qIng5jvKLIcB8B7kTFMpDmjbwpp0371Qfz6zly8OzTZprKhydoVZu9myeqaOBgb9agvhTfRdzo0LHrsL0hKJcSIRD4y_xFhWqY4KNMJ_wHi4huwr1YvNqg2eNBeu1sb8g4ZsZ8Td0mvPEeSaLkSaQzZoviQP9knV9vVeWEqrojHpzN9vJ1ckyFdhHcs9iaodxDHfbeRdusmoCQ2XtiRnmXxW-TqN1-97vundXHEPiEUwMI8eU06bluOb-MGpuE2hTQhJtLMoWqWfS_GSK6sQb6kT_bUf7J9vHzzo1aXS4HNPDVkb_7WjYBrqLk6tvWdYFnp9C4DSZ42XU_HmlsAjpf_1lyDplxh9uIXFAl2PtSxWvLIrMSz6cSSDOKld2mbwgiXhT8exbklticT2OiFo_U5bF6-2XqYp0bvpVFh_cms-0D76_9X5hovg0vO1T0H1WKqfrQSprSWZ76S6CS1y0E2E9O815GC7SPAFD0SIRt2kmV0xIh254S7uX9oSrzGvwOwFjOtx8LU3zjpL6g1mLYUGxWiwPhtPl2_SNGBCcK0MhVh8Gl4zs5Ijj7HM7jbyjdxwxr-PkJdqfj14skZdXbBZ8H0uH07GycueXDngbjDyCI6XWigsIV_-n52kO1r8CDYi2wU2z2JPL2fqsPeBvoZQzAaQ==";
 		PaymentPageResponse response = paymentHandler.unpackResponse(respString);
-
 		System.out.println("Response: mid=" + response.getMid() + ", orderid=" + response.getOrderId() + ", status="
 				+ response.getStatus());
 
