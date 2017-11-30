60f6ed53194f9a32544472229b0d9091d72c169e
Payal Ghadge <pghadge@worldlinenordics.com>
2017-11-30 06:42:01 +0100

    dummy change to trigger build

--- src/main/java/com/digitalriver/worldpayments/api/PaymentRequestBuilder.java
+++ src/main/java/com/digitalriver/worldpayments/api/PaymentRequestBuilder.java
@@ -438,7 +438,6 @@ public class PaymentRequestBuilder {
 
         //FIXME: This creates an unnecessary nvp. Needed for validation purposes only
         ParameterAnnotationHelper.mapObjectToNvp(paymentRequest);
-
         return paymentRequest;
     }
 }
