139289055408469eb22be2fd2dfd234f50934d4f
ahagerback <aake.hagerback@worldline.com>
2018-10-31 09:50:30 +0100

    PAYAPIJAVA-35 added missing files

---
+++ src/main/java/com/worldline/payments/api/InitAuthenticationResponse.java
@@ -0,0 +1,42 @@
+package com.worldline.payments.api;
+
+public class InitAuthenticationResponse extends AbstractPaymentResponse {
+
+    public Long getMid() {
+        return mid;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+   
+    public String getStatus() {
+        if (status == null || status.isEmpty()) {
+            if (clientAnswerCode == 0) {
+                return status = "OK";
+            } else {
+                return status = "NOK";
+            }
+        } else {
+            return status;
+        }
+    }
+
+    public String getAuthenticationStatus() {
+        return authenticationStatus;
+    }
+
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    public String getRedirectParameters() {
+        return redirectParameters;
+    }
+    
+    public String getRedirectMethod() {
+        return redirectMethod;
+    }
+
+}
