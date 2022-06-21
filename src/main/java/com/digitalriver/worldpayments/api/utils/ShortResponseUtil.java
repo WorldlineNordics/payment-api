c30edd6ab3e3ba9407d8c8d81a4b35de2b2cd3d5
Prithviraj Pawar <prpawar@worldlinenordics.com>
2022-06-21 13:08:54 +0530

    converted output to string

--- src/main/java/com/digitalriver/worldpayments/api/utils/ShortResponseUtil.java
+++ src/main/java/com/digitalriver/worldpayments/api/utils/ShortResponseUtil.java
@@ -1,25 +1,34 @@
 package com.digitalriver.worldpayments.api.utils;
 
+import java.io.UnsupportedEncodingException;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.util.Base64;
 
+import com.digitalriver.worldpayments.api.security.SecurityHandlerException;
 import com.digitalriver.worldpayments.api.utils.CryptoUtils.CryptoException;
 
 public class ShortResponseUtil {
 
 	private Base64Utils iBase64Encoder;
 	
-	public void decodeWithBase64(String PPSResponse, PublicKey publicKey, byte[] sign) throws CryptoException
+	public String decodeWithBase64(String PPSResponse, PublicKey publicKey, byte[] sign) throws CryptoException 
 	{
 //		iBase64Encoder.decode(PPSResponse);
 		byte[] response = PPSResponse.getBytes();
 		byte[] decodedResponse = Base64.getDecoder().decode(response);
-		
+		String plainText;
 		try {
 			CryptoUtils.verifySignature(publicKey, response, sign);
 		} catch (CryptoException e) {
 			throw e;
 		}
+		try {
+			 plainText = decodedResponse.toString();
+		} catch (Exception excep) {
+			// Should never happen
+			throw excep;
+		}
+		return plainText;
 	}
 }
