bf5d61b729be982f67aff6c033965f6a1cc63517
Prithviraj Pawar <prpawar@worldlinenordics.com>
2022-07-13 09:38:45 +0200

    done review comments

--- src/test/java/com/digitalriver/worldpayments/api/security/ShorterResponse_V6Test.java
+++ src/test/java/com/digitalriver/worldpayments/api/security/ShorterResponse_V6Test.java
@@ -8,18 +8,14 @@ import org.junit.Test;
 public class ShorterResponse_V6Test {
 
 	@Test
-	public void ShorterResonseDecryptV6Test() {
-		String DECRYPTED = "ACA=PP_1657636376606;ACB=2;ACC=debit;";
-		// This string has been encrypted in the PPS with the corresponding
-		// private key there and is known to represent the content in DECRYPTED.
-		String ENCRYPTED = "BgAAA-wE9qN3j5Uk2JmxpJAlFMT_oyheGbH-NBSW6ZPc8wUaKrMjUDXxKCTubYMtBtnCOnT59D2MoVWSnkXNOjYyWEC_ZzoJ8jER_geHZpLs3TtvPhJmDnWvgpUrQitiW6q2QFows_BkNP-lolyDhsH3vRBNBNZkZ9Ua930Pn8aO477xsVlboZD6QnXo57qa8VAdtr3dSzh_LolmJvyiw20AS6P48nWgSf97QAyJZUrdI7D8MVBYp_3rzvzbdkUJytbMHCMwHO9Jmei72MrcQNM97s-dlV1xzsxkmFnv58ALIVK3NJKVA-aSGa_oyUKWYon9bv8xaKSqEazWJ09ozB_SeqHYQUNBPVBQXzE2NTc2MzYzNzY2MDY7QUNCPTI7QUNDPWRlYml0Ow==";
-
+	public void ShorterResonseDecodedV6Test() {
+		String DECODED = "ACA=PP_1657636376606;ACB=2;ACC=debit;";
+		String ENCODED = "BgAAA-wE9qN3j5Uk2JmxpJAlFMT_oyheGbH-NBSW6ZPc8wUaKrMjUDXxKCTubYMtBtnCOnT59D2MoVWSnkXNOjYyWEC_ZzoJ8jER_geHZpLs3TtvPhJmDnWvgpUrQitiW6q2QFows_BkNP-lolyDhsH3vRBNBNZkZ9Ua930Pn8aO477xsVlboZD6QnXo57qa8VAdtr3dSzh_LolmJvyiw20AS6P48nWgSf97QAyJZUrdI7D8MVBYp_3rzvzbdkUJytbMHCMwHO9Jmei72MrcQNM97s-dlV1xzsxkmFnv58ALIVK3NJKVA-aSGa_oyUKWYon9bv8xaKSqEazWJ09ozB_SeqHYQUNBPVBQXzE2NTc2MzYzNzY2MDY7QUNCPTI7QUNDPWRlYml0Ow==";
 		
-
 		com.digitalriver.worldpayments.api.utils.ShortResponseUtil decryptor = new com.digitalriver.worldpayments.api.utils.ShortResponseUtil(
-				new com.digitalriver.worldpayments.api.security6.JKSKeyHandlerV6("src/test/resources/new_merchant.jks", "merchant_ks",
+			new com.digitalriver.worldpayments.api.security6.JKSKeyHandlerV6("src/test/resources/new_merchant.jks", "merchant_ks",
 						"1578446540", "drwp_cert"));
-		String decr = decryptor.decodeWithBase64(ENCRYPTED);
-		assertThat(DECRYPTED, equalTo(decr));
+		String deco = decryptor.decodeWithBase64(ENCODED);
+		assertThat(DECODED, equalTo(deco));
 	}
 }
