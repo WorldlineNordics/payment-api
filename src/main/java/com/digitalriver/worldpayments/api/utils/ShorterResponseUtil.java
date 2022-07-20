8c8833b23642ec44962005f7b2f2ccd06ead17c6
Nitin Chavhan <nchavhan@worldlinenordics.com>
2022-07-20 07:52:55 +0200

    corrections

--- src/main/java/com/digitalriver/worldpayments/api/utils/ShorterResponseUtil.java
+++ src/main/java/com/digitalriver/worldpayments/api/utils/ShorterResponseUtil.java
@@ -11,14 +11,14 @@ import com.digitalriver.worldpayments.api.security.SecurityHandlerException;
 import com.digitalriver.worldpayments.api.security6.JKSKeyHandlerV6;
 import com.digitalriver.worldpayments.api.utils.CryptoUtils.CryptoException;
 
-public class ShortResponseUtil {
+public class ShorterResponseUtil {
 
 	private static final String ENCODING_UTF_8 = "UTF-8";
 	
 	private final Base64Utils iBase64Encoder;
 	private final JKSKeyHandlerV6 iKeyHandlerV6;
 
-	public ShortResponseUtil(JKSKeyHandlerV6 iKeyHandlerV6) {
+	public ShorterResponseUtil(JKSKeyHandlerV6 iKeyHandlerV6) {
 		this.iKeyHandlerV6 = iKeyHandlerV6;
 		this.iBase64Encoder = new Base64Utils();
 	}
@@ -30,13 +30,14 @@ public class ShortResponseUtil {
 		envelope = iBase64Encoder.decode(ppsResponse);
 		byte[] cipherText = new byte[envelope.length - 261];
 		PublicKey drwpPublicKey;
+		byte[] plainText;
 
 		if (envelope[0] == 6) {
 			System.arraycopy(envelope, 1, netgiroCertSerialNo, 0, 4);
 			System.arraycopy(envelope, 5, signature, 0, 256);
 			System.arraycopy(envelope, 261, cipherText, 0, cipherText.length);
 			drwpPublicKey = iKeyHandlerV6.getDrwpPublicKey(new BigInteger(netgiroCertSerialNo).longValue());
-
+			
 			try {
 				if (!CryptoUtils.verifySignature(drwpPublicKey, cipherText, signature)) {
 					throw new SecurityHandlerException("Signature does not match!");
@@ -45,9 +46,16 @@ public class ShortResponseUtil {
 				throw new SecurityHandlerException("Failed to verify signature!", e);
 			}
 		}
+		
+		try {
+			plainText = CryptoUtils.unzip(cipherText);
+		} catch (CryptoException e) {
+			throw new SecurityHandlerException(
+					"Failed to unzip plaintext!", e);
+		}
 
 		try {
-			return new String(cipherText, ENCODING_UTF_8);
+			return new String(plainText, ENCODING_UTF_8);
 		} catch (UnsupportedEncodingException e) {
 			throw new SecurityHandlerException("Failed to convert result to UTF-8", e);
 		}
--- src/test/java/com/digitalriver/worldpayments/api/security/ShorterResponse_V6Test.java
+++ src/test/java/com/digitalriver/worldpayments/api/security/ShorterResponse_V6Test.java
@@ -9,13 +9,13 @@ public class ShorterResponse_V6Test {
 
 	@Test
 	public void ShorterResonseDecodedV6Test() {
-		String DECODED = "ACA=PP_1657636376606;ACB=2;ACC=debit;";
-		String ENCODED = "BgAAA-wE9qN3j5Uk2JmxpJAlFMT_oyheGbH-NBSW6ZPc8wUaKrMjUDXxKCTubYMtBtnCOnT59D2MoVWSnkXNOjYyWEC_ZzoJ8jER_geHZpLs3TtvPhJmDnWvgpUrQitiW6q2QFows_BkNP-lolyDhsH3vRBNBNZkZ9Ua930Pn8aO477xsVlboZD6QnXo57qa8VAdtr3dSzh_LolmJvyiw20AS6P48nWgSf97QAyJZUrdI7D8MVBYp_3rzvzbdkUJytbMHCMwHO9Jmei72MrcQNM97s-dlV1xzsxkmFnv58ALIVK3NJKVA-aSGa_oyUKWYon9bv8xaKSqEazWJ09ozB_SeqHYQUNBPVBQXzE2NTc2MzYzNzY2MDY7QUNCPTI7QUNDPWRlYml0Ow==";
+		String DECODED = "ACA=PP_1658295222169;ACB=OK;ACC=Debit;";
+		String ENCODED = "BgAAA-wXf10G91CizAKsGJH5P48MjKnvTH0kM7VuQPabSbhrZPyosvCo5t6CJza3GebzYpoLJH3QkdQ25VnxbfQxQcDyV3gT-0uc59ykhS5FKy26Azb0tJ3DVrUh38BfCcoWWvKgxjHIkV5xXhVzT1XDcabbcUw6dI204M62w5Clw4lOE_n0bO1j_4KfLhG0DMz9VbEZNdLOTvR4eAq3zOQIVTJYAbdUtQC7W7Rts2-ck39b2T00LCoEcR5i0nv1mFb6lY6PeK_D2evcMXJOzhzY8GBXcRNiV7t8ifPPlaw8_Kp93EHJoxlOPFCAmcpDicJcor7ZfJTu9gS0jf1rJiEB2BOBH4sIAAAAAAAAAHN0drQNCIg3NDO1MLI0NTIyMjSztHZ0drL19wZSzrYuqUmZJdYAQaGe8iYAAAA=";
 		
-		com.digitalriver.worldpayments.api.utils.ShortResponseUtil decryptor = new com.digitalriver.worldpayments.api.utils.ShortResponseUtil(
+		com.digitalriver.worldpayments.api.utils.ShorterResponseUtil decryptor = new com.digitalriver.worldpayments.api.utils.ShorterResponseUtil(
 			new com.digitalriver.worldpayments.api.security6.JKSKeyHandlerV6("src/test/resources/new_merchant.jks", "merchant_ks",
 						"1578446540", "drwp_cert"));
-		String deco = decryptor.decodeWithBase64(ENCODED);
-		assertThat(DECODED, equalTo(deco));
+		String decoded = decryptor.decodeWithBase64(ENCODED);
+		assertThat(DECODED, equalTo(decoded));
 	}
 }
